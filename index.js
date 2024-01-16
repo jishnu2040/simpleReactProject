0fa71b72ca70f9946b8afd4d93077e2c1ff6bfd9
jishnuraj rs <jishnuraj017@gmail.com>
2024-01-16 16:45:10 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const heading = React.createElement("div", {id:"paranet"}, ("div", {id:"child"}, ("h3", {id:"heading"}, "this is h1 tag under parent!")));
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading)
